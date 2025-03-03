10e74c4cd73e92f986ebb1a90a5f8c8abdfe7856
Kirill Novichkov <sufirhavat@gmail.com>
2025-03-03 16:52:22 +0700

    added some optimization

--- src/main/kotlin/Chat.kt
+++ src/main/kotlin/Chat.kt
@@ -5,16 +5,13 @@ data class Chat(
     private var idMessage: Long = 1
     private var readMessage: Boolean = false // True, если все сообщения в чате прочитаны и false, если нет
 
-    fun addMessage(mess: String) { // Добавление сообщения в чат
-        val message = Message(
-            mess,
-            idMessage
-        )
-        idMessage += 1
-        messageList.add(message)
+    fun addMessage(mess: String): Boolean { // Добавление сообщения в чат
+        messageList.add(Message(mess, idMessage))
+        idMessage++
+        return true
     }
 
-    fun delMessage(idMessage: Long): Boolean { //Удаляет сообщение по его идентификатору
+    fun deleteMessage(idMessage: Long): Boolean { //Удаляет сообщение по его идентификатору
         return messageList.removeAll { it.getId() == idMessage }
     }
 
@@ -23,7 +20,8 @@ data class Chat(
     }
 
     override fun toString(): String { // Возвращает список сообщений в чате
-        return "Собеседник $idChatPartner " +if (messageList.size != 0) messageList.toString() + "\n"
-        else "Сообщений нет\n"
+        return "Собеседник $idChatPartner - " +
+                if (messageList.isNotEmpty()) messageList.toString() + "\n"
+                else "Сообщений нет\n"
     }
 }
--- src/main/kotlin/ChatService.kt
+++ src/main/kotlin/ChatService.kt
@@ -5,25 +5,12 @@ class ChatService(
         return listOfChat
     }
 
-    fun sendMessage(idChatPartner: Long, mess: String): Boolean { //Создает новый чат и добавляет
-        for (chat in listOfChat) {                                //первое сообщение или
-            if (chat.getIdChat() == idChatPartner) {              //добавляет сообщение к существующему чату
-                chat.addMessage(mess)
-                return true
-            }
-        }
-        val chat = Chat(idChatPartner)
-        chat.addMessage(mess)
-        listOfChat.add(chat)
-        return false
+    fun sendMessage(idChatPartner: Long, mess: String): Boolean {
+        return listOfChat.find { it.getIdChat() == idChatPartner }?.addMessage(mess) ?:
+        listOfChat.add(Chat(idChatPartner).apply { addMessage(mess) })
     }
 
-    fun deleteMessage(idChatPartner: Long, idMessage: Long): Boolean { // Удаляет сообщение в конкретном
-        for (chat in listOfChat) {                                     // чате
-            if (chat.getIdChat() == idChatPartner) {
-                return chat.delMessage(idMessage)
-            }
-        }
-        return false
+    fun deleteMessage(idChatPartner: Long, messageId: Long): Boolean { // Удаляет сообщение в чате с собеседником
+        return listOfChat.find { it.getIdChat() == idChatPartner }?.deleteMessage(messageId) ?: false
     }
 }
